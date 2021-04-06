749135569f13e9dd2a2ad4f2b826f75b98e57e1b
1801684 <a.chumber2@wlv.ac.uk>
2021-04-06 18:03:52 +0100

    Fixed issue with image not loading in heroku app - Was added in css, now added in js

--- client/src/components/feature/login/Login.js
+++ client/src/components/feature/login/Login.js
@@ -1,6 +1,7 @@
 //Parent For Login Section
 import LoginForm from './auth/LoginForm';
 import CreateAccount from './create/CreateAccount';
+import FormImage from '../../../assets/images/form-image.jpg';
 import './login.css';
 import Logo from '../../../assets/images/Logo.svg';
 import { useState } from 'react'; 
@@ -17,7 +18,11 @@ const Login = ({ isLoggingIn }) => {
     return(
         <div className="form-wrapper">
             <Link to="/" title="Click to go to Home page"><img src={Logo} alt="Blogs Logo" className="login-logo" /></Link>
-            <div className="form-image"></div>
+            <div className="form-image-container">
+                <img src={ FormImage } className="form-image" rel="preload" 
+                        loading="lazy"
+                        alt="Bookshelf filled with books"/>
+            </div>
             {isLogin ? <LoginForm changePage={ handleChange } history={ history }/> : <CreateAccount changePage={ handleChange } history={ history }/> }
         </div>
     );
